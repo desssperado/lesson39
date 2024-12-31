2983d09cdbbb3eb1f93600373e2ae463ccde4474
desssperado <gamzatgasanov0@gmail.com>
2024-12-31 14:51:29 +0300

    Add files via upload

---
+++ module_8_2.py
@@ -0,0 +1,36 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+
+    for number in numbers:
+        try:
+            result += number
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {number}')
+
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    try:
+        sum_result, incorrect_data_count = personal_sum(numbers)
+        count_valid_numbers = len(numbers) - incorrect_data_count
+
+        if count_valid_numbers > 0:
+            average = sum_result / count_valid_numbers
+            return average
+        else:
+            return 0
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
